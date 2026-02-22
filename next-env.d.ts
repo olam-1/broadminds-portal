aa8d5a2d47f9ba5d0311ec921e1676c399001635
Moshood Olagunju <adekunle1984@gmail.com>
2026-02-22 13:05:01 +0100

    Fix supabaseClient path alias

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,8 +1,5 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
-<<<<<<< HEAD
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
-=======
->>>>>>> a5b9ce593025faca7273154c6591ed4cb02be261
